660e72315220f33dc28eff7255e0ef9b53b9a7ba
Neeraj Pokala <73011282+Neerajpokala@users.noreply.github.com>
2024-06-27 16:30:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -67,7 +67,7 @@ if prompt := st.chat_input(placeholder="What is this data about?"):
         st.stop()
 
     llm = ChatOpenAI(
-        temperature=0, model="gpt-4-turbo", openai_api_key=openai_api_key, streaming=True
+        temperature=0, model="gpt-3.5-turbo-0125", openai_api_key=openai_api_key, streaming=True
     )
 
     pandas_df_agent = create_pandas_dataframe_agent(
